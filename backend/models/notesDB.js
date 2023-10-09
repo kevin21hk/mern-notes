0311c7b37828346021ad2ed067d740b6daab63ce
Kevin <111733224+kevin21hk@users.noreply.github.com>
2023-10-10 01:48:50 +0800

    create mongoose db schema for notesDB

---
+++ backend/models/notesDB.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const NoteSchema = mongoose.Schema({
+    noteTitle: {
+        type: String,
+    },
+    noteData: {
+        type: String,
+        required: true
+    },
+    notePublicity: {
+        type: Boolean,
+        default: true
+    },
+    notePassword: {
+        type: String,
+        minlength: 6
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+      }
+})
+
+const Notes = mongoose.model('Notes', NoteSchema)
+
+module.exports = Notes
