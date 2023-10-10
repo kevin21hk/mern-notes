9850e0b9be49d29973b3f57ce0cbba3c793bb589
Kevin <111733224+kevin21hk@users.noreply.github.com>
2023-10-11 03:30:18 +0800

    hash notePassword using  bcrypt with 10 saltRounds

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -1,24 +1,28 @@
 const mongoose = require('mongoose')
 const Notes = require('../models/notesDB')
+const bcryptController = require('./bcryptPassword')
 
 module.exports = {
 
-    createNote: (req,res) => {
-        const newData = req.body
-        
+    createNote: async(req,res) => {
+        const {notePassword, ...otherData } = req.body
+        const hashedPassword = await bcryptController.hashPassword(notePassword)
+        const newData = {
+            notePassword: hashedPassword,
+            ...otherData
+        }
+        console.log(hashedPassword)
         if (newData.noteTitle === ''){
             newData.noteTitle = 'Untitled'
         }
-
         const newNote = new Notes(newData)
-        newNote.save()
-        .then(() => {
+        try {
+            await newNote.save()
             console.log("Data saved to DB")
             res.status(200).json()
-        })
-        .catch((err) => {
-            console.error('Error saving data to DB:', err);
-            res.status(500).json({ error: 'Failed to save data to DB' });
-        })
+        } catch (err) {
+            console.error('Error saving data to DB:', err)
+            res.status(500).json({ error: 'Failed to save data to DB' })
+        }
     }
 }
