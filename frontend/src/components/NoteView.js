8836968a5d775149a18497fbf5972bf13b5bbe8a
Kevin <111733224+kevin21hk@users.noreply.github.com>
2023-10-21 14:19:57 +0800

    modify to show password input box based on certain criterias

--- frontend/src/components/NoteView.js
+++ frontend/src/components/NoteView.js
@@ -17,7 +17,7 @@ const NoteView = () => {
         disabledId : false,
         disabledPath : false
     })
-
+    const [isNoteAuth, setIsNoteAuth] = useState(false)
     const idRef = useRef(null)
     const pathRef = useRef(null)
     const currentPath = window.location.href
@@ -38,15 +38,19 @@ const NoteView = () => {
     fetchNote()
     }, [id])
 
+    const handleNoteAuth = () => {
+        setIsNoteAuth(true)
+      }
+
     if (loading) {
         return <div>Loading...</div>
       }
 
     if (!note) {
-        return <div>No note found</div>;
+        return <div>No note found</div>
       }
 
-      note.createdAt = new Date(note.createdAt);
+      note.createdAt = new Date(note.createdAt)
       const options = {
         weekday: 'short',
         day: '2-digit',     
@@ -63,7 +67,6 @@ const NoteView = () => {
         var copiedEl
         var disabledEl
         var ref
-        
     
         if ((el === 'note-id' && copyDisabled.disabledId) || (el === 'note-path' && copyDisabled.disabledPath))  {
             return
@@ -107,52 +110,72 @@ const NoteView = () => {
 
     return(
         <>
-        <Password/>
-        <h1>{note.noteTitle}</h1>
-        <span>{formattedDate} - <span className="publicity-span">{note.notePublicity === 'Private' ? 'Private (password protected)' : 'Public (viewed by everyone)'}</span></span>
+        { 
+            note.notePublicity === 'Private' 
+            && 
+            !isNoteAuth 
+            && 
+            <Password 
+            id={id} 
+            onNoteAuth={handleNoteAuth}
+            /> 
+        }
 
-        <form className="form-note">
-            <div className="form-note-wrapper">
-                <textarea 
-                    className="form-note-textarea" 
-                    name="noteData"
-                    rows="15" 
-                    maxLength="2000"
-                    value={note.noteData}
-                    readOnly
-                />
-            </div>
+        { 
+            (note.notePublicity !== 'Private' 
+            || 
+            ( note.notePublicity === 'Private' 
+            && 
+            isNoteAuth ))
+            && 
+            (<>
+            <h1>{note.noteTitle}</h1>
+            <span>{formattedDate} - <span className="publicity-span">{note.notePublicity === 'Private' ? 'Private (password protected)' : 'Public (viewed by everyone)'}</span></span>
+
+            <form className="form-note">
+                <div className="form-note-wrapper">
+                    <textarea 
+                        className="form-note-textarea" 
+                        name="noteData"
+                        rows="15" 
+                        maxLength="2000"
+                        value={note.noteData}
+                        readOnly
+                    />
+                </div>
+                
+                <div className="note-info-wrapper">
+                        <label htmlFor="note-id">Note ID:</label>
+                        <input 
+                                type="text" 
+                                name="note-id"
+                                id="note-id"
+                                className="note-id"
+                                ref={idRef}
+                                value={note.noteHash} 
+                                onClick={(el) => (copyText(el.target.name))}
+                                readOnly                           
+                        />
+                        <label htmlFor="note-id">{copiedData.copiedId ? 'Copied to clipboard' : ''}</label>
+                </div>
             
-            <div className="note-info-wrapper">
-                    <label htmlFor="note-id">Note ID:</label>
-                    <input 
-                            type="text" 
-                            name="note-id"
-                            id="note-id"
-                            className="note-id"
-                            ref={idRef}
-                            value={note.noteHash} 
-                            onClick={(el) => (copyText(el.target.name))}
-                            readOnly                           
-                    />
-                    <label htmlFor="note-id">{copiedData.copiedId ? 'Copied to clipboard' : ''}</label>
-            </div>
-           
-            <div className="note-info-wrapper">
-                    <label htmlFor="note-path">Note Path:</label>
-                    <input 
-                            type="text" 
-                            name="note-path"
-                            id="note-path"
-                            className="note-path"
-                            ref={pathRef}
-                            value={`${currentPath}`} 
-                            onClick={(el) => (copyText(el.target.name))}
-                            readOnly                           
-                    />
-                    <label htmlFor="note-path">{copiedData.copiedPath ? 'Copied to clipboard' : ''}</label>
-            </div>
-        </form>
+                <div className="note-info-wrapper">
+                        <label htmlFor="note-path">Note Path:</label>
+                        <input 
+                                type="text" 
+                                name="note-path"
+                                id="note-path"
+                                className="note-path"
+                                ref={pathRef}
+                                value={`${currentPath}`} 
+                                onClick={(el) => (copyText(el.target.name))}
+                                readOnly                           
+                        />
+                        <label htmlFor="note-path">{copiedData.copiedPath ? 'Copied to clipboard' : ''}</label>
+                </div>
+            </form>
+            </>)
+        }
         </>
     )
 }
