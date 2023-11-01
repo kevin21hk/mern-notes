74e63ebc87754fbed27277743eb83763a4bf3c37
Kevin <111733224+kevin21hk@users.noreply.github.com>
2023-11-01 17:50:33 +0800

    allow logo to navigate to create-note route

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import logoImg from '../images/mern-notes-logo.png'
 
 const Header = () => {
 
     return(
         <>
-        <img src={logoImg} className="logo-image" alt="logo" />
+        <Link to='/create-note/'>
+            <img src={logoImg} className="logo-image" alt="logo" />
+        </Link>
         </>
     )
 }
