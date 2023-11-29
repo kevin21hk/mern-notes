492b349994da30a515997c0d10f095120913ccab
Kevin <111733224+kevin21hk@users.noreply.github.com>
2023-11-30 01:51:44 +0800

    add eslint devtool for backend formatting

--- backend/models/notesDB.js
+++ backend/models/notesDB.js
@@ -1,37 +1,37 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const NoteSchema = mongoose.Schema({
-    noteHash: {
-        type: String,
-        required: true
-    },
-    noteTitle: {
-        type: String,
-    },
-    noteData: {
-        type: String,
-        required: true
-    },
-    notePublicity: {
-        type: String,
-        default: true
-    },
-    notePassword: {
-        type: String,
-        minlength: 6,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    failedLoginAttempts: {
-        type: Number,
-        default: 0
-    },
-    lockoutTime: {
-        type: Date
-    }
-})
+  noteHash: {
+    type: String,
+    required: true
+  },
+  noteTitle: {
+    type: String
+  },
+  noteData: {
+    type: String,
+    required: true
+  },
+  notePublicity: {
+    type: String,
+    default: true
+  },
+  notePassword: {
+    type: String,
+    minlength: 6
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  failedLoginAttempts: {
+    type: Number,
+    default: 0
+  },
+  lockoutTime: {
+    type: Date
+  }
+});
 
-const Notes = mongoose.model('Notes', NoteSchema)
+const Notes = mongoose.model('Notes', NoteSchema);
 
-module.exports = Notes
+module.exports = Notes;
